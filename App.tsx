988c9473a4256593df621af0c65e73ed8ed00dd4
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2025-06-23 15:51:57 +0600

    add health check API to check server status when app mounts

--- App.tsx
+++ App.tsx
@@ -5,15 +5,28 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import AppNavigator from './src/navigation/AppNavigator';
 import {store, persistor} from './src/store';
 import './src/i18n';
+import Toast from 'react-native-toast-message';
+import axiosInstance from './src/axiosInstance';
+import { URLS } from './src/constants/urls';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    axiosInstance.get(URLS.healthCheck)
+      .then(res => {
+        console.log('Health check success:', res.data);
+      })
+      .catch(err => {
+        console.log('Health check error:', err);
+      });
+  }, []);
+
   return (
     <Provider store={store}>
       {/* PersistGate is used to persist the state to AsyncStorage */}
@@ -21,6 +34,7 @@ function App(): React.JSX.Element {
         {/* SafeAreaProvider is used to handle the safe area insets */}
         <SafeAreaProvider>
           <AppNavigator />
+          <Toast />
         </SafeAreaProvider>
       </PersistGate>
     </Provider>
