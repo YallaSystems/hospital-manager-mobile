e82ebff5d5cf4ac2dafa29899fad5e05eab04269
Rashedul Hassan <rashedul@outlook.com>
2025-06-25 11:03:40 +0100

    chore (cleanup) :remove wdio and detox scripts

--- e2e/firstTest.e2e.js
+++
@@ -1,9 +0,0 @@
-describe('HomeScreen', () => {
-  beforeAll(async () => {
-    await device.launchApp();
-  });
-
-  it('should show welcome text', async () => {
-    await expect(element(by.text('Welcome to Hospital Manager'))).toBeVisible();
-  });
-});
