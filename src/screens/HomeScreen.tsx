ea68faa98160aed22b7bae1cd94006749e668bae
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2025-06-29 20:59:59 +0600

    show user's full name on logged in user's home page

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,15 +3,21 @@ import { View, Text, StyleSheet } from 'react-native';
 import { useHomeViewModel } from '../viewmodels/useHomeViewModel';
 import { useTranslation } from 'react-i18next';
 import { COLORS } from '../constants/colors';
+import { useSelector } from 'react-redux';
+import { selectUserFullName } from '../store/selectors/userSelectors';
 
 const HomeScreen = () => {
   const { t } = useTranslation();
+  const userFullName = useSelector(selectUserFullName);
 
   // Currently no state, but hook is ready for future logic
   useHomeViewModel();
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{t('welcome', 'Welcome to Hospital Manager')}</Text>
+      <Text style={styles.text}>{t('welcome', 'Welcome to Hospital Manager') }</Text>
+      {userFullName && (
+        <Text style={styles.name}>{userFullName}</Text>
+      )}
     </View>
   );
 };
@@ -26,6 +32,11 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 24,
     fontWeight: 'bold',
+  },
+  name: {
+    fontSize: 20,
+    marginTop: 10,
+    color: COLORS.primary,
   },
 });
 
