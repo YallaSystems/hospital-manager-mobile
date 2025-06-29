93b67c97933a0965e3a40b1c822a7b254c036aad
Rashedul Hassan <rashedul@outlook.com>
2025-06-29 11:01:28 +0100

    fix: remove y folder

--- test/specs/home.e2e.js
+++
@@ -1,6 +0,0 @@
-describe('Home Screen', () => {
-  it('should show welcome text', async () => {
-    const welcomeText = await $('~welcomeText'); // testID in your RN app
-    await expect(welcomeText).toBeDisplayed();
-  });
-});
