68d89204cca0d327eac511970b9f080942e30c9e
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2025-07-01 21:17:48 +0600

    remove Health check service

--- App.tsx
+++ App.tsx
@@ -6,27 +6,16 @@
  */
 
 import './src/wdyr';
-import React, { useEffect, useState } from 'react';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {Provider} from 'react-redux';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
 import AppNavigator from './src/navigation/AppNavigator';
-import {store} from './src/store';
+import { store } from './src/store';
 import './src/i18n';
-import axiosInstance from './src/axiosInstance';
-import { URLS } from './src/constants/urls';
-import { Modal, View, Text, StyleSheet, Button } from 'react-native';
-import { useTranslation } from 'react-i18next';
-import { performHealthCheck } from './src/services/healthCheckService';
-import MaintenanceModal from './src/components/MaintenanceModal';
+import { StyleSheet } from 'react-native';
 import AuthInitializer from './src/components/AuthInitializer';
 
 function App(): React.JSX.Element {
-  const [isMaintenanceModalVisible, setMaintenanceModalVisible] = useState(false);
-  const { t } = useTranslation();
-
-  useEffect(() => {
-    performHealthCheck(setMaintenanceModalVisible, t);
-  }, []);
 
   return (
     <Provider store={store}>
@@ -35,13 +24,6 @@ function App(): React.JSX.Element {
         <AuthInitializer>
           <AppNavigator />
         </AuthInitializer>
-        <MaintenanceModal
-          visible={isMaintenanceModalVisible}
-          onRetry={() => performHealthCheck(setMaintenanceModalVisible, t)}
-          title={t('healthCheck.maintenanceTitle', 'System Under Maintenance')}
-          message={t('healthCheck.maintenanceMessage', 'We are currently performing maintenance. Please try again later.')}
-          retryButtonText={t('healthCheck.retryButton', 'Retry')}
-        />
       </SafeAreaProvider>
     </Provider>
   );
